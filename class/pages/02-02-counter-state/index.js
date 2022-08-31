8e7ae4e530af2811f0a5cad5cc517164f58f73e8
smyn95 <tlsalduszz@gmail.com>
2022-08-31 10:15:06 +0900

    리액트 State

---
+++ class/pages/02-02-counter-state/index.js
@@ -0,0 +1,23 @@
+import {useState} from 'react'
+
+export default function CounterStatePage() {
+  const [count, setCount] = useState(0) //기존방식 : let count = 0
+
+  function onClickCountUp(){
+    setCount(count + 1) //기존방식 : count = count + 1
+  }
+ 
+   function onClickCountDown(){
+    console.log(count)
+    setCount(count - 1) //기존방식 : count = count - 1
+  }
+   
+  return(
+    <>
+      <div>{count}</div>
+      <button onClick={onClickCountUp}>카운트 올리기</button>
+      <button onClick={onClickCountDown}>카운트 내리기</button>
+    </>
+  )
+
+}
